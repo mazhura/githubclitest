ece9bcfb76a7846211bd2d0590d60d6fdffec07a
Mykhailo Mazhura <mazhura1@gmail.com>
2024-01-23 00:02:11 +0200

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
+
+        </div>
+    </body>
+</html>
